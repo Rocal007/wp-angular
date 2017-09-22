d1842fd5bafb1337884fb946d03ba31cf9e21ace
Rocal007 <r.sauer007@gmail.com>
2017-09-22 16:34:10 +0200

    Website Component, News Component

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,6 +1,8 @@
 import { WpNewsService } from './../services/wp-news/wp-news.service';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-news',
@@ -9,19 +11,48 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class NewsComponent implements OnInit {
 
-  news: any; 
+  news: any;
+  data: any; 
   
       constructor(private service: WpNewsService) {
        
           };
 
- 
+          getData(id1) {
+            
+            let newsList = this.service.getNew([id1]);
+            console.log(newsList);
+            return newsList;
+        }
 
 ngOnInit() {
-  this.service.getNews()
-  .subscribe(response => {
-    console.log(response.json());
-    this.news = response.json(); 
-    //this.news =this.news.feed.data;
-    console.log(this.news);    
-})}}
+  let self: NewsComponent = this;
+  let b = this.service.getCachedNews();
+  //console.log (b);
+
+  if (b == null) {
+      this.service.getNews()
+          .subscribe(
+
+
+              function(news) {
+                  self.news = news;
+                  self.service.setCachedNews(self.news);
+                  console.log(self.news);
+              },
+
+              error => {
+                  alert('An error occured');
+                  console.log(error);
+
+              },
+
+
+          )
+
+  } else {
+      this.news = b;
+  }
+}
+
+};
